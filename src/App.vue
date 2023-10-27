2fafe466a1ff8c8820ed29df5ea2f5c7cbe94604
Pablo Cesar Agudelo Castro <pcagudelo@gmail.com>
2023-10-27 10:54:00 -0500

    Version 1.0 Movilidad nacional lista

---
+++ src/App.vue
@@ -0,0 +1,425 @@
+<script setup>
+
+  import {ref, computed}  from "vue";
+  // Import Bootstrap and BootstrapVue CSS files (order is important)
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+ // import countries from "../src/components/json/paises.json";
+  import rangosSalariales from "../src/components/rangosSalariales.js"
+
+
+ let calculadoraTipoBeneficiario=ref(0);
+ const salarioMinimo= parseInt(1160000);
+
+ let salarioDocente= ref(0);
+
+  //Realiza un switch entre el el SMLV y un salario Docente
+  //[Estado: Works]
+ const salarioBeneficiario= computed(()=>{
+ 
+  if( calculadoraTipoBeneficiario.value== 1 ){
+     return salarioMinimo;  
+         
+    }else {
+      return salarioDocente.value
+      }
+  });
+  
+  let dias=ref(1);
+  let pernoctar=ref(0);
+  let distancia=ref(0);
+  const viaticos= ref([]);
+  let transporteAereo= ref(0);
+  let transporteTerrestre= ref(0);
+  let nombreBeneficiario=ref("Juan Julio Salgado Espinosa");
+ 
+  //movilidad internacionla: Pendiente
+  let movilidadInternacional=ref(0);
+  const ext2={paises:[302,301,303,344,6,391],continente:[3] } ;
+  const ext3={paises:[340], contienente:[1,2,7,8]} 
+  const ext1={paises:[], contienente:[4,5,6]}
+  
+  //Determinar con base al pais seleccinado
+  //que tipo de movilidad internacional es
+  function determinarExtension(ext){
+
+    if(ext2.paises.includes(ext.pais)){
+      return 2
+    }else if(ext2.continente.includes(ext.continente)){
+      return 2
+    }else if(ext3.paises.includes(ext.pais)){
+      return 3
+    } else if(ext3.contienente.includes(ext.continente)){
+      return 3
+    }else return 1
+
+  }
+  
+  let destinoInternacional=ref(343);
+
+  //Segun la distancio y si pasa la noche en otro lugar se define el multiplo por el cual se calcula
+  //[Estado: Works]
+  const multiplo=computed(()=>{
+        let multiplo;
+        //Viaticos plenos
+        if(pernoctar.value && distancia.value){
+                multiplo=1;
+        }
+        //Viaticos reducidos
+        if(!pernoctar.value && distancia.value){
+            multiplo=0.5;
+        }
+        //Zona de influencia
+        if(!pernoctar.value && !distancia.value){
+            multiplo=0.25;
+        }
+        //Zona de influencia pernoctar
+        if(pernoctar.value && !distancia.value){
+            multiplo=0.5;
+        }
+        return parseFloat(Number(multiplo));
+  });
+  //Dentro de las tablas obtiene los valores maximos de movilidad 
+  //[Estado: Works]
+  const defRango = computed(()=>{
+
+    let retorno=ref([])
+    for(const value of rangosSalariales ){
+        
+      if(salarioBeneficiario.value >=value.valorMin && salarioBeneficiario.value <= value.valorMax){
+
+        retorno.value.push(
+          parseInt(value.pesos),
+          parseInt(value.ext1),
+          parseInt(value.ext2),
+          parseInt(value.ext3)
+        )
+        break
+      }
+
+    }
+
+    return retorno
+
+
+  });
+
+  //EN CONSTRUCCION
+  const tipoViatico= computed(()=>{
+
+    if(movilidadInternacional.value==false){
+
+      return defRango.value[0];
+    }else{
+      
+      return  defRango[determinarExtension(destinoInternacional)];
+      
+    }
+
+  }); 
+ //Añade elemetos preconfigurados a viaticos
+  //[Estado: Refactorizar]
+  const addViaticos=(dias, pernoctar, distancia, valor)=>{
+        let ingresar = {};
+        ingresar.dias= dias;
+        ingresar.pernoctar = pernoctar;
+        ingresar.distancia = distancia;
+        ingresar.valor= valor;
+        ingresar.sub= valor*dias
+        viaticos.value.push(ingresar);
+  }
+ //Eliminar un registro de los viaticos
+ //[Estado: Works]
+  const  elimarItem=(item)=>{viaticos.value.splice(item,1)}
+  //Suma El total de los dias ya que seran usados para el transporte interno.
+ //[Estado: Works]
+  const totalViaticosDias= computed(()=>{
+      let total=0;
+      for(let viatico of viaticos.value ){
+        total+= viatico.dias;
+      }
+      return total;
+  });
+ // 
+ //[Estado: Works]
+  const transporteInterno= computed(()=>{
+    if(totalViaticosDias.value && totalViaticosDias.value>0){
+      return totalViaticosDias.value*(0.02*salarioBeneficiario.value)
+    }
+  });
+
+//Suma El total de los valores de los viaticos, sera usado para el total favorable.
+ //[Estado: Works]
+ const totalViaticosValor= computed(()=>{
+    let total=0;
+    for(let viatico of viaticos.value ){
+      total+= parseFloat(Number(viatico.sub));
+    }
+    return total
+  });
+
+  
+//Suma El total de los valores de los viaticos, sera usado para el total favorable.
+ //[Estado: ]
+const totalViaticosTransporte = computed(()=>{
+
+  if(parseFloat(totalViaticosValor.value)>0 && parseFloat(transporteInterno.value)>0){ 
+    return parseFloat(totalViaticosValor.value+transporteAereo.value+transporteTerrestre.value+transporteInterno.value)
+  } else return 0
+
+});
+
+
+function moneda(variable){
+
+  const valor= new Intl.NumberFormat("es-CO",{style: 'currency',minimumFractionDigits: 2, currency: 'COP'}).format(variable); 
+
+  return valor;
+}
+
+const viaticosMoneda= computed(()=>{
+  return moneda(totalViaticosTransporte.value)
+});
+
+let datosPersistencia=ref([]);
+function presistencia(event){
+  event.preventDefault(); 
+  let temporal = {};
+  temporal.totalDias= totalViaticosDias.value;
+  temporal.nombreBeneficiario = nombreBeneficiario.value;
+  temporal.totalValor = totalViaticosTransporte.value;
+  datosPersistencia.value.push(temporal);
+  viaticos.value=[];
+  //this.$refs.vitaCalcForm.reset();
+}
+</script>
+
+<template>
+  <form class="container" ref="vitaCalcForm">
+
+    <div class="row row-cols-auto">
+        <div class="col-6 "><!-- BLOQUE 1 Datos del Beneficiario -->
+          <h5>Datos del Beneficiario</h5>
+          
+          <h6>Tipo de Benefeficiario</h6>
+          <div class="form-check form-switch">      
+              <label class="form-check-label" for="tipoBeneficiario">Estudiante</label>
+              <input class="form-check-input"  type="checkbox" v-model="calculadoraTipoBeneficiario">
+          </div>
+
+          <div class="row">
+            <div class="col-lg-6 col-sm-12">
+              <div class="form-floating">
+                <input type="text" class="form-control" v-model="nombreBeneficiario">
+                <label for="nombreBeneficiario">Nombre Beneficiario</label>
+              </div>
+            </div>
+
+            <div class="col-lg-6 col-sm-12">
+            <!-- Segun tipo de beneficioario se mostrara este contenido -->
+              <div>
+                <div class="form-floating" >
+                    <input 
+                      type="number" 
+                      class="form-control" 
+                      id="calculadoraSalarioBeneficiaro"  
+                      v-model="salarioDocente" 
+                      :disabled="calculadoraTipoBeneficiario==1">
+                    <label for="calculadoraSalarioBeneficiaro">Salario</label>
+                </div>         
+              </div>                  
+            </div>
+
+            <div v-if="calculadoraTipoBeneficiario==1">
+                    El salario de los estudiantes se toma como 1 SMLV
+            </div>
+          </div>
+          <br>
+          <!-- Datos del Beneficiario -->        
+        
+          <div><!-- Composicion de Detalle de los viaticos: CALCULO DE LOS VIATICOS -->
+            <h5>Calculo de viaticos</h5>
+            <div class="form-check form-switch" hidden>
+              <label class="form-check-label" for="movilidadInternaciional">Internacional</label>
+              <input class="form-check-input" type="checkbox" name="" id="movilidadInternaciional" v-model="movilidadInternacional" disabled>
+            </div>
+            
+            <!--EN DESARROLO Internacional -->
+            <div v-if="movilidadInternacional==1">
+              <label for="destinoInternacional">Seleecione el Pais</label>
+                <input list="countries" name="" id="destinoInternacional" v-model="destinoInternacional">
+              <datalist id="countries">
+                <option v-for="country in countries" :key="country.codigo_pais" :value="country.nombre_pais">{{}}</option>           
+              </datalist>
+            </div>
+              
+            <div  class="row"><!--CARRO DE COMPRAS VIATICOS-->
+              <div class="col-6" v-show="movilidadInternacional==0">
+                <div class="form-check form-switch"><!---Pernoctar-->
+                 <label class="form-check-label" for="calculadoraMovilidadInternorPernoctar">Pernoctacion</label>
+                 <input class="form-check-input"  type="checkbox" id="calculadoraMovilidadInternorPernoctar" v-model="pernoctar">
+                </div>
+              
+                <div class="form-check form-switch"><!---Distancia-->
+                  <label class="form-check-label" for="calculadoraMovilidadInteriorDistancia"> Distancia Superior a 20 KM ?</label>
+                  <input class="form-check-input" type="checkbox" id="calculadoraMovilidadInteriorDistancia" v-model="distancia">
+                </div>
+              </div>
+              <div class="col-6">
+                <div class="row">
+                  <div class="col-4">
+                    <div class="form-floating">
+                      <input type="number" class="form-control" id="calculadoraMovilidadInteriorDias" placeholder="" v-model="dias">
+                      <label for="calculadoraMovilidadInteriorDias">Dias</label>
+                    </div>
+                  </div>
+
+                  <div class="col-2">
+                    <button
+                     type="button"
+                     class="btn btn-outline-primary btn-sm"
+                     @click="addViaticos(dias, pernoctar, distancia, defRango.value[0]*multiplo )"
+                     > Add</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+          </div>
+
+                    
+           <!-- Tipos de transporte -->
+           <br>
+          <h5>Datos de transporte </h5>
+
+          <div class="row">
+            <div class="col-lg-4 col-sm-6">
+              <div class="form-floating"><!--TRANSPORTE INTERNO: READONLY-->
+                <input type="number" class="form-control" id="transporteInterno"  :value="transporteInterno" disabled >
+                <label for="transporteInterno">Interno</label>
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-sm-6"> 
+              <div class="form-floating"><!--TRANSPORTE TERRESTRE-->
+                <input type="number" class="form-control" id="transporteTerrestre"  v-model="transporteTerrestre" >
+                <label for="transporteTerrestre">Terrestre</label>
+              </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+              <div class="form-floating"><!--TRANSPORTE AEREO-->
+                <input type="number" class="form-control" id="transporteAereo"  v-model="transporteAereo">
+                <label for="transporteAereo">Aereo</label>
+              </div>  
+            </div>
+          </div>
+
+          <br>  
+          <div>
+            <h6>Tatal Viaticos y gastos de transporte: <span class="text-success">{{ viaticosMoneda }}</span></h6>
+          </div>
+
+          <div class="d-grid gap-2">
+          <button class="btn btn-success" @click  ="presistencia($event)">Nuevo Calculo</button>
+          </div>
+
+        
+        </div>
+        
+        <div class="col-6"><!-- DETALLE VIATICOS -->
+          <div><!-- Renderizado de los viaticos -->
+            <h5>Detalle de los viaticos</h5>
+            <table class="table table-sm">
+
+              <thead>
+                  <tr class="table-gray">  
+                    <th class="text-center">#</th>             
+                  <th>Dias</th>
+                  <th hidden>Movilidad</th>
+                  <th>
+                   Perno..
+                  </th>
+                  <th>Dist...</th>
+                  <th><img src="./components/icons/bootstrap-icons-test/currency-dollar.svg" alt="Pesos" width="20" height="20">Diario</th>
+                  <th><img src="./components/icons/bootstrap-icons-test/substack.svg" alt="Pesos" width="20" height="20"> Subtotal</th>
+                  <th>Eliminar</th>
+                </tr>
+              </thead>
+              <tbody class="table-group-divider">
+                <!--Renderizado de lista -->   
+                <tr v-for="(viatico, index) in viaticos">
+                  <th scope="row" class="text-center">{{index+1}}</th> 
+                  <td class="text-center">{{ viatico.dias }}</td>
+                  <td hidden>{{movilidadInternacional ? "internacional" : "nacional :"}}</td>
+                  <td>{{ viatico.pernoctar? "Sí": "No" }}</td>
+                  <td>{{ viatico.distancia? "Sí": "No" }} </td>
+                  <td>{{ moneda(viatico.valor) }}</td>
+                  <td>{{ moneda(viatico.sub) }}</td>
+                  <td class="text-center">                                
+                    <img src="./components/icons/bootstrap-icons-test/trash.svg" alt="eliminar" width="20" height="20" class="text-danger text-center" fill="currentColor" @click="elimarItem(index)" >
+                  </td>
+                </tr>
+                <!--Totales de tabla-->
+                <tr class="">
+                  <td colspan="6" class="text-center">Total: {{ moneda(totalViaticosValor)}}</td>              
+                </tr>
+              </tbody>
+            </table>
+          </div>
+         
+        
+        </div>
+
+    </div>
+    <div class="row">
+      <!-- CONSTRUCCION VIATICOS -->
+      <div class="col">   
+      </div>
+      <!-- DETALLE DE LOS DATOS OBTENIDOS -->
+      <div class="col" hidden>
+        <h5>Detale de los calculos</h5>
+        <p>Tipo de beneficiario {{  calculadoraTipoBeneficiario }}</p>
+        <p>Salario para calculo: {{salarioBeneficiario}}</p>        
+        <p>Va a percnotar ? {{ pernoctar}}</p>
+        <p>Cual es la distancia ? {{distancia}}</p>      
+        <p>El multiplo es  {{ multiplo }}</p>
+        <p>Rangos Salariales: {{ defRango }}</p> 
+        <p>Valor disponible para este funcionario Pesos {{defRango.value[0]*multiplo }}</p>
+        <p>Valor dependiendo de la movilidad: {{tipoViatico}}</p>
+        <p>Tatal Viaticos y gastos de transporte: {{ viaticosMoneda }}</p>
+      </div>
+ 
+
+    </div>
+  </form>
+
+  
+  <div class="container"><!-- Muestra de persistencia -->
+    <br>
+    <h5>Historico </h5>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col">Nombre</th>
+          <th scope="col" class="text-center">Total Dias</th>
+          <th scope="col" class="text-center">Total Valor</th>
+        </tr>
+      </thead>
+        <tbody class="table-group-divider">
+          <tr v-for="(dato, index) in datosPersistencia">
+            <td>{{ index +1}}</td>
+            <td>{{ dato.nombreBeneficiario }}</td>
+            <td class="text-center">{{ dato.totalDias }}</td>
+            <td class="text-center">{{  moneda(dato.totalValor) }}</td>
+          
+          </tr>
+        </tbody>
+      
+    </table>
+
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/rangosSalariales.js
@@ -0,0 +1,15 @@
+const rangosSalariales=[
+{"valorMin":0,        "valorMax": 1317595, "pesos": 119503,"ext1":80,"ext2":100,"ext3":140},
+{"valorMin":1317596,  "valorMax": 2070476, "pesos": 163323,"ext1":110,"ext2":150,"ext3":220},
+{"valorMin":2070477,  "valorMax": 2764819, "pesos": 198167,"ext1":140,"ext2":200,"ext3":300},
+{"valorMin":2764820,  "valorMax": 3506799, "pesos": 230588,"ext1":150,"ext2":210,"ext3":320},
+{"valorMin":3506800,  "valorMax": 4235186, "pesos": 264787,"ext1":160,"ext2":240,"ext3":350},
+{"valorMin":4235187,  "valorMax": 6387301, "pesos": 298863,"ext1":170,"ext2":250,"ext3":360},
+{"valorMin":6387302,  "valorMax": 8927247, "pesos": 363014,"ext1":180,"ext2":260,"ext3":370},
+{"valorMin":8927248,  "valorMax": 10599866,"pesos": 489708,"ext1":200,"ext2":265,"ext3":380},
+{"valorMin":10599867, "valorMax": 13048829,"pesos": 636612,"ext1":270,"ext2":315,"ext3":445},
+{"valorMin":13048830, "valorMax": 15778536,"pesos": 770045,"ext1":350,"ext2":390,"ext3":510},
+{"valorMin":15778537, "valorMax":157785370,"pesos": 906844,"ext1":440,"ext2":500,"ext3":640}
+];
+
+export default rangosSalariales;
